4c002105960eeda7b138db9e60ac2cb4b8e7453f
Daniel Studart <danielstudart22@gmail.com>
2023-10-09 10:50:27 -0300

    Criando Componente novo

---
+++ src/Primeiro.jsx
@@ -0,0 +1,7 @@
+const Primeiro = () => {
+    return (<div>
+                <h5>Mini titulo</h5>
+                <p>Meu Texto</p>
+            </div>);
+};
+export default Primeiro;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Primeiro from './Primeiro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Primeiro/>
+    <Primeiro/>
+    <Primeiro/>
   </React.StrictMode>
 );
 
