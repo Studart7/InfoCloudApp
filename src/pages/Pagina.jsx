494e4e717dc473dffef3813721688d7721bc0c49
Daniel Studart <danielstudart22@gmail.com>
2023-11-20 12:38:15 -0300

    Adicionando funcionalidade de ter mais de uma página

--- src/pages/Pagina.jsx
+++ src/pages/Pagina.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Pagina = () => {
   return (
     <div>
       <h2>Esta é Minha Página</h2>
       <p>Bem-vindo à minha página!</p>
+      <Link to="/tabela">Va para tabela</Link>
     </div>
   );
 };
